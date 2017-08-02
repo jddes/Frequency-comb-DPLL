59d73fbb563d42680c69e741c14eaecf4ac8fade
jddes <jean-daniel.deschenes@gel.ulaval.ca>
2017-08-01 20:31:03 -0400

    No real change, just recompiled.

--- Firmware Vivado Project/redpitaya.runs/impl_1/runme.sh
+++ Firmware Vivado Project/redpitaya.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='D:/Repo/Frequency-comb-DPLL - Python 3/Firmware Vivado Project/redpitaya.runs/impl_1'
+HD_PWD='D:/Repo/Frequency-comb-DPLL/Firmware Vivado Project/redpitaya.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
