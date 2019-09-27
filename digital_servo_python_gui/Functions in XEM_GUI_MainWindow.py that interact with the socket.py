37f707afc30b285ebc598ad267b084a76feb2fe1
jddes <octosigconsulting@gmail.com>
2019-09-27 17:43:27 +0200

    RP_PLL_test actually goes through the socket now; still need to add useful test cases with exceptions, disconnects, etc

--- digital_servo_python_gui/Functions in XEM_GUI_MainWindow.py that interact with the socket.py
+++ digital_servo_python_gui/Functions in XEM_GUI_MainWindow.py that interact with the socket.py
@@ -75,15 +75,41 @@ chkLockClickedEvent(self): done
 ------------- next up:
 
 -Test and make sure that our RP_PLL (and controller?) object is nicely behaved when the socket throws exceptions.
+    -Build a mock:
+        -server runs in a separate QThread (in the test file)
+        -server receives signals:
+        -next reply adds X seconds of delay
+        -next reply adds X seconds of delay then closes connection
+        -close connection immediately (are there multiple ways of closing the connection?)
+
+        -handling of socket errors:
+            -on first socket error, throw a CommsLoggeableException, set valid_socket = False, notify whoever will be in charge of a reconnection timeout
+            -Next calls to read or send: when valid_socket = True, throw CommsException and do nothing else
+
+            -somewhere is a reconnection mecanism that isn''t obnoxious
+
+            
+
+-Make sure that every other file that interacts either with the socket or RP_PLL device handles comms exceptions correctly.
     -FreqCounterWindow
     -SettingsWindow
--Make sure that every other file that interacts either with the socket or RP_PLL device handles comms exceptions correctly.
+    -look around to make sure there are not others too! ditherSettings?
 
 -Make some kind of higher level test, where we can actually open the GUI?  One way would be to create a mock TCP server that returns reasonable values for each read
     -Could start by reading the monitor-tcp or RP_PLL code.  There are a few packet types that we need to handle (reg_write, reg_read (2 types each), read_buffer.)
     -This is will be very helpful in the future...
+    -could also be part of future automated tests
 
 -Do we forge ahead and start dealing with RP_PLL, or work on a higher level test?
+
+-could do a pretty low level test with RP_PLL and either a mock socket object, or a mock socket server.
+-decision: lets make it a mock socket server!
+    -This way we can either do it super simple and just test RP_PLL+simple_server by itself (in the same thread even) to make sure it handles tcp events nicely
+    -or we can make a much more sophisticated mock server, which would actually enable using the GUI without a real device connected.
+
+
+
+
 
 
 
--- digital_servo_python_gui/RP_PLL_test.py
+++ digital_servo_python_gui/RP_PLL_test.py
@@ -2,17 +2,9 @@ from PyQt5 import QtGui, Qt, QtCore, QtWidgets
 import sys
 import os
 import time
-
+import struct
 import numpy as np
-
-# to start a thread:
-        # self.resolveSignsThread = ResolveSignsThread(self)
-
-        # self.resolveSignsThread.signsResolveComplete.connect(self.signsResolveComplete)
-        # self.resolveSignsThread.statusUpdate.connect(self.statusUpdate)
-        
-        # self.newFreqSignal.connect(self.resolveSignsThread.newFreqMeasurement)
-        # self.resolveSignsThread.start()
+import pytest
 
 from AsyncSocketComms import AsyncSocketServer
 from AsyncSocketComms import AsyncSocketClient
@@ -20,10 +12,17 @@ from AsyncSocketComms import AsyncSocketClient
 import RP_PLL
 
 class MonitorTCP_mock():
+
+    invalid_read = -1111 # default value if memory has not been written to before
+
+
     def __init__(self):
         self.reply_latency = 0
 
         self.memory_buffer = bytearray(RP_PLL.RP_PLL_device.MAX_SAMPLES_READ_BUFFER)
+        self.regs = {}
+        self.data_to_send_back = bytearray()
+
 
         self.magic_bytes_to_handler = {
             RP_PLL.RP_PLL_device.MAGIC_BYTES_WRITE_REG: self.write_reg_handler,
@@ -32,25 +31,21 @@ class MonitorTCP_mock():
         }
 
     def parse_buffer(self, data_buffer):
-        # parse the buffer, similar to what monitor-tcp does!
-        # TODO: This needs to consume the buffer, otherwise the data will just accumulate.
+        # parse the buffer, similar to what monitor-tcp does.
+        # This needs to consume the buffer, otherwise the data will just accumulate.
 
-        if len(data_buffer) >= 4:
-            magic_bytes = struct.unpack('I', data_buffer)
-            handler_func = self.magic_bytes_to_handler.get(magic_bytes, None)
-            if handler_func is None:
-                print("Error: unrecognized magic_bytes 0x%x" % magic_bytes)
-            else:
-                (data_to_send_back, bytes_consumed_from_buffer) = handler_func(data_buffer)
-                
-                data_buffer[0:bytes_consumed_from_buffer] = bytearray()
-                return data_to_send_back
+        if len(data_buffer) < 4:
+            return None
 
-        return None
-        # if len(data_buffer) > 0:
-        #     print("in MonitorTCP_mock::parse_buffer(): data_buffer=%s. Clearing." % data_buffer)
-        #     data_buffer[:] = bytearray()
-        # # return data_buffer # Do we need to return it to be able to modify it? I think not, but I am not sure...
+        (magic_bytes,) = struct.unpack('I', data_buffer[:4])
+        handler_func = self.magic_bytes_to_handler.get(magic_bytes, None)
+        if handler_func is None:
+            print("Error: unrecognized magic_bytes 0x%x" % magic_bytes)
+        else:
+            (data_to_send_back, bytes_consumed_from_buffer) = handler_func(data_buffer)
+            print("parse_buffer(): data_to_send_back=%s, bytes_consumed_from_buffer=%d" % (repr(data_to_send_back), bytes_consumed_from_buffer))
+            data_buffer[0:bytes_consumed_from_buffer] = bytearray()
+            return data_to_send_back
 
     # Handlers return a tuple containing: (data_to_send_back, bytes_consumed_from_buffer)
     def write_reg_handler(self, data_buffer):
@@ -62,8 +57,8 @@ class MonitorTCP_mock():
         if len(data_buffer) < bytes_consumed:
             return (None, 0)
 
-        (magic_bytes, addr, data) = struct.unpack('=III', data_buffer)
-        self.regs[addr/4] = data
+        (magic_bytes, addr, data) = struct.unpack('=III', data_buffer[:bytes_consumed])
+        self.regs[int(addr/4)] = data
         return (None, bytes_consumed)
 
     def read_reg_handler(self, data_buffer):
@@ -75,8 +70,17 @@ class MonitorTCP_mock():
         if len(data_buffer) < bytes_consumed:
             return (None, 0)
 
-        (magic_bytes, addr, reserved) = struct.unpack('=III', data_buffer)
-        return (self.regs[addr/4], bytes_consumed)
+        (magic_bytes, addr, reserved) = struct.unpack('=III', data_buffer[:bytes_consumed])
+
+        try:
+            data = self.regs[int(addr/4)]
+        except KeyError:
+            data = self.invalid_read
+
+        bytes_to_send = struct.pack('=i', data)  # signed or unsigned doesn't matter here
+
+
+        return (bytes_to_send, bytes_consumed)
 
     def read_buf_handler(self, data_buffer):
         words_consumed = 3
@@ -89,28 +93,32 @@ class MonitorTCP_mock():
 
         return (self.memory_buffer, bytes_consumed)
 
+    # Removes data from the start of a bytearray and returns it
+    def remove_from_queue(self, data_array, bytes_to_remove):
+        if len(data_array) < bytes_to_remove:
+            raise Exception("Not enough bytes in buffer.")
+        data = data_array[:bytes_to_remove]
+        data_array[:] = bytearray()
+        return data
 
-    # def write_Zynq_register_32bits(self, absolute_addr, data_32bits, bSigned=False):
-    #     self.validate_address(absolute_addr)
-    #     packet_to_send = struct.pack(self.type_to_format_string[bSigned], self.MAGIC_BYTES_WRITE_REG, absolute_addr, int(data_32bits) & 0xFFFFFFFF)
-    #     self.send(packet_to_send)
+    # Add data to the end of a bytearray:
+    def add_to_queue(self, data_array, bytes_to_add):
+        if bytes_to_add is not None:
+            data_array += bytes_to_add
 
-    # def read_Zynq_register_32bits(self, absolute_addr, bIsAXI=False):
-    #     self.validate_address(absolute_addr)
-    #     packet_to_send = struct.pack('=III', self.MAGIC_BYTES_READ_REG, absolute_addr, 0)  # last value is reserved
-    #     self.send(packet_to_send)
-    #     return self.read(4)
+    # send/read Interface to mock without using a socket
+    def send_mock(self, packet_to_send):
+        packet_to_send = bytearray(packet_to_send) # need bytearray since it is mutable, as opposed to bytes()
+        print("packet_to_send=%s, type=%s" % (repr(packet_to_send), type(packet_to_send)))
+        data_to_send_back = self.parse_buffer(packet_to_send)
+        len2 = lambda x: 0 if x is None else len(x)
+        print("send_mock(), before: size=%d, to add: %d" % (len2(self.data_to_send_back), len2(data_to_send_back)))
+        self.add_to_queue(self.data_to_send_back, data_to_send_back)
+        print("send_mock(), after: size=%d" % len2(self.data_to_send_back))
 
-    # def read_Zynq_buffer_int16(self, number_of_points):
-    #     if number_of_points > self.MAX_SAMPLES_READ_BUFFER:
-    #         number_of_points = self.MAX_SAMPLES_READ_BUFFER
-    #         print("number of points clamped to %d." % number_of_points)
-
-    #     packet_to_send = struct.pack('=III', self.MAGIC_BYTES_READ_BUFFER, self.FPGA_BASE_ADDR, number_of_points)    # last value is reserved
-    #     self.send(packet_to_send)
-    #     return self.read(int(2*number_of_points))
-
-
+    def read_mock(self, bytes_to_read):
+        return self.remove_from_queue(self.data_to_send_back, bytes_to_read)
+        
 class ServerThread(QtCore.QThread):
     statusUpdate = QtCore.pyqtSignal(str)
     dataReceived = QtCore.pyqtSignal(str)
@@ -124,27 +132,27 @@ class ServerThread(QtCore.QThread):
         self.server = AsyncSocketServer(port_number=5000, bStartListening=False)
         self.monitor_tcp = MonitorTCP_mock()
         self.bReadEnable = False
+        self.server.bVerbose = False
 
     def run(self):
         print("ServerThread::run()")
 
         while 1:
-            if self.bReadEnable:
-                self.server.run(bParseBufferIntoLines=False)
-
-                if len(self.server.read_buffer) > 0:
-                    self.monitor_tcp.parse_buffer(self.server.read_buffer)
-                    print("in ServerThread::run(): self.server.read_buffer=%s" % self.server.read_buffer)
-                else:
-                    self.monitor_tcp.parse_buffer(self.server.read_buffer)
-                # # if we can reply, observe self.monitor_tcp.reply_latency setting!
-                # if (we can reply):
-                #     time.sleep(self.monitor_tcp.reply_latency)
-                #     # do the actual reply!
-
-            print(".")
+            print(".", end="")
             time.sleep(100e-3)
-        pass
+
+            if not self.bReadEnable:
+                continue
+
+            self.server.run(bParseBufferIntoLines=False)
+
+            data_to_send_back = self.monitor_tcp.parse_buffer(self.server.read_buffer)
+
+            # reply after the prescribed delay
+            if data_to_send_back is not None:
+                print("run(): data_to_send_back = %s" % repr(data_to_send_back))
+                time.sleep(self.monitor_tcp.reply_latency)
+                self.server.sock_conn.sendall(data_to_send_back)
 
     @QtCore.pyqtSlot()
     def startListening(self):
@@ -178,7 +186,6 @@ def start_qt():
         # print("QCoreApplication already running.")
 
     return app
-
 
 class objSocketMock(QtCore.QObject):
     startListening = QtCore.pyqtSignal()
@@ -214,46 +221,51 @@ class objSocketMock(QtCore.QObject):
         print("timer")
         self.thread.exit()
         QtCore.QCoreApplication.instance().quit()
+        raise "timerQuit(): Test took too long."
 
-def test_socketMock():
-    # print("creating socket")
+def check_readreg(dev):
+    reg_in = 10; reg_addr = 100*4; reg_out = 0
+    dev.write_Zynq_register_uint32(address_uint32=reg_addr, data_uint32=reg_in)
+    reg_out = dev.read_Zynq_register_uint32(address_uint32=reg_addr)
+    assert reg_in == reg_out
+    assert dev.read_Zynq_register_int32(address_uint32=2334234+2) == MonitorTCP_mock.invalid_read
 
+# this essentially tests our MonitorTCP_mock class
+@pytest.mark.skip(reason="just for speed, remove this skip later")
+def test_monitorTCP_mock():
+    dev = RP_PLL.RP_PLL_device()
+    # connect mocks
+    monitor_tcp = MonitorTCP_mock()
+    dev.send = monitor_tcp.send_mock
+    dev.read = monitor_tcp.read_mock
 
-    app = start_qt()
-
-    print("before objSocketMock()")
-    server_thread = objSocketMock()
-    print("after objSocketMock()")
-
-
-    timer = QtCore.QTimer()
-    timer.singleShot(3000, server_thread.timerQuit)
-
-    # thread = ServerThread()
-    # thread.start()
-
-    # print("before server_thread.thread.run()")
-    # server_thread.thread.start()
-    # print("after server_thread.thread.run()")
-
-    client = AsyncSocketClient(5000)
-    print("client connected.")
-
-    for k in range(10):
-        client.send_text('test')
-        print("sent test")
-        time.sleep(0.2)
-
-
-    app.exec_()
-    assert(0)
-
+    # actual test
+    check_readreg(dev)
 
 def test1():
     app = start_qt()
+
+    server_thread = objSocketMock()
+    time.sleep(0.1)
+
+    timerMaxTestDuration = QtCore.QTimer()
+    timerMaxTestDuration.singleShot(1000, server_thread.timerQuit)
+
+    dev = RP_PLL.RP_PLL_device()
+
+    dev.OpenTCPConnection(HOST="127.0.0.1")
+    assert dev.valid_socket
+
+    # actual test
+    server_thread.setReplyLatency.emit(1.0)
+    check_readreg(dev)
+
+@pytest.mark.skip(reason="used only while developping tests")
+def test_socket():
+    app = start_qt()
+
     server_thread = objSocketMock()
 
-    # Timeout timer for the whole test:
     timer = QtCore.QTimer()
     timer.singleShot(3000, server_thread.timerQuit)
 
@@ -263,8 +275,7 @@ def test1():
     for k in range(10):
         client.send_text('test')
         print("sent test")
-        time.sleep(0.2)
-
+        time.sleep(0.1)
 
     app.exec_()
     assert(0)
@@ -276,7 +287,8 @@ if __name__ == '__main__':
     # server.server.bVerbose = False
     # server.run()
 
-    test_socketMock()
+    # test_socketMock()
+    test1()
 
     pass
 
