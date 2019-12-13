72b3412a17a8325255b25b6ca1b1e1286866657d
jddes <octosigconsulting@gmail.com>
2019-12-12 20:38:17 -0500

    upgrade reg_event to recv and save reg values

--- digital_servo_python_gui/RegistersDisplay.py
+++ digital_servo_python_gui/RegistersDisplay.py
@@ -11,8 +11,7 @@ class EventTypes(Enum):
     written = auto()
     changed = auto()
 
-RegMarkingInfo = namedtuple('RegMarkingInfo', ['unmark_time', 'field_name', 'event_type'])
-
+RegEventInfo = namedtuple('RegEventInfo', ('field_name', 'event_type'))
 
 class RegisterState():
     def __init__(self, reg_definitions):
@@ -22,11 +21,14 @@ class RegisterState():
             EventTypes.changed: 1,
         }
 
-        self.unmark_queue = deque()
+        self.unmark_queue = dict() # keys are (field_name, event_type) tuples, and values is the expiration time
 
         self.reg_definitions = reg_definitions
         # build addr -> field_name lookup table for faster lookup at runtime
         self.name_from_addr = {reg_info.addr: fieldname for (fieldname, reg_info) in self.reg_definitions.items()}
+
+        # start with unknown register values
+        self.reg_values = {key:None for key in self.reg_definitions.keys()}
 
     def setMarkCallback(self, callback):
         """ This callback will get called with individual register info
@@ -42,28 +44,30 @@ class RegisterState():
         current_time = time.perf_counter()
 
         list_del = []
-        for index, reg_info in enumerate(self.unmark_queue):
+        for reg_info, unmark_time in self.unmark_queue.items():
             # check if this register is not yet ready to get unmarked:
-            if reg_info.unmark_time > current_time:
+            if unmark_time > current_time:
                 continue
             # time to unmark this register.
             self.mark_reg(reg_info.field_name, reg_info.event_type, bMark=False)
 
-            # remove item from the queue
-            list_del.append(index)
+            # remove item from the queue once we are done looping through
+            list_del.append(reg_info)
 
         # do the deletion after we are done iterating through the deque itself (otherwise it throws an error)
-        for index in list_del:
-            del self.unmark_queue[index]
+        for key in list_del:
+            self.unmark_queue.pop(key)
 
-    def reg_event(self, addr=None, field_names=None, event_type=None):
+    def reg_event(self, addr=None, field_names=None, event_type=None, values=None):
         """ call this when one or more registers has had an event (read, written, changed).
         Specify either the address(es) or the field name of the register.
         Specify a list of addresses or names if there are multiple regs
         being reported at the same time.
 
         event_type must be one of the valid values
-        in the EventTypes enum """
+        in the EventTypes enum 
+
+        values must be the register(s) new value(s)"""
 
         # first need to figure out if arguments use addr or field_names:
         if addr is not None:
@@ -76,17 +80,21 @@ class RegisterState():
             field_names_internal = field_names
 
         # now do the actual work:
-        map_if_list(partial(self._reg_event_single, event_type), field_names_internal)
+        map_if_list(partial(self._reg_event_single, event_type), field_names_internal, values)
 
-    def _reg_event_single(self, event_type, field_name):
+    def _reg_event_single(self, event_type, field_name, value):
         """ Actual function that does the work, called from reg_event.
         Only handles one register at a time. """
-        # TODO: mark this register as reg at current time (change color), scheduled to be unmarked at time+Treg
-        reg_info = RegMarkingInfo(unmark_time=time.perf_counter()+self.mark_timeouts[event_type],
-                                  field_name=field_name,
-                                  event_type=event_type)
+
+        # update the GUI only if this value is actually different than it was last time:
+
+
+        # mark this register as reg at current time (change color)
+        reg_info = RegEventInfo(field_name, event_type)
         self.mark_reg(field_name, event_type, bMark=True)
-        self.unmark_queue.append(reg_info)
+        # schedule the expiration of this marking at a later time:
+        unmark_time = time.perf_counter()+self.mark_timeouts[event_type]
+        self.unmark_queue[reg_info] = unmark_time
 
 # End class RegisterState
 
@@ -99,3 +107,4 @@ def map_if_list(func, *args):
 
 
 
+print("TODO: CLICKING ON A REGISTER OPENS A SUMMARY WINDOW: VALUE, and all other parameters (copy-pasteable)")
