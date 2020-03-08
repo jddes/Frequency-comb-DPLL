fcce9160cfc317471ce21e673a7a13a17d0fe2a0
jddes <jean-daniel.deschenes@gel.ulaval.ca>
2020-03-08 13:42:17 -0400

    Formatting in new_vhdl_module only

--- Firmware_Vivado_Project/redpitaya.srcs/sources_1/new_vhdl_module.vhd
+++ Firmware_Vivado_Project/redpitaya.srcs/sources_1/new_vhdl_module.vhd
@@ -3,38 +3,36 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
-entity new_vhdl_module  is
+entity new_vhdl_module is
 Generic (
-	GENERIC1			: integer := 3000;
-	GENERIC2 		: integer := 64;
-	GENERIC3 		: integer := 64
+    DATA_WIDTH    : integer := 16;
+    COUNTER_WIDTH : integer := 32
 );
 port (
-	rst 								: in  std_logic;
-	clk									: in  std_logic
+    clk : in  std_logic
 
 
 
-	);
+    );
 end entity;
 
 architecture Behavioral of new_vhdl_module is
 
-	--type state_type is (STATE_IDLE, STATE_POS_OFFSET, STATE_ZERO1, STATE_NEG_OFFSET, STATE_ZERO2);
-	--signal FSM_state      : state_type := STATE_IDLE;
+    --type state_type is (STATE_IDLE, STATE_POS_OFFSET, STATE_ZERO1, STATE_NEG_OFFSET, STATE_ZERO2);
+    --signal FSM_state      : state_type := STATE_IDLE;
 
 begin
 
-	---- FSM process example
-	--process (clk)
-	--begin
-	--	if rising_edge(clk) then
-	--		case FSM_state is
-	--			when STATE_IDLE =>
-	--			when others =>
-	--				FSM_state <= STATE_IDLE;
-	--		end case;
-	--	end if;
-	--end process;
+    ---- FSM process example
+    --process (clk)
+    --begin
+    --  if rising_edge(clk) then
+    --      case FSM_state is
+    --          when STATE_IDLE =>
+    --          when others =>
+    --              FSM_state <= STATE_IDLE;
+    --      end case;
+    --  end if;
+    --end process;
 
 end;
