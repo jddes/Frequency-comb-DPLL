c5e2947950e16102736a1e2741b42d9bad28a6ee
jddes <jean-daniel.deschenes@gel.ulaval.ca>
2021-05-06 16:47:36 -0400

    fix errors in last commit

--- digital_servo_python_gui/common.py
+++ digital_servo_python_gui/common.py
@@ -22,7 +22,8 @@ def findMostLikelyLANBroadcastIPAddress():
     """
     listAddr = socket.getaddrinfo(socket.gethostname(), None)
     ipv4Addresses = [sockaddr[0] for (family, _, _, _, sockaddr) in listAddr if family == socket.AF_INET]
-    findMostLikelyLANBroadcastIPAddress_inner(listAddr)
+    print(locals())
+    return findMostLikelyLANBroadcastIPAddress_inner(ipv4Addresses)
 
 def findMostLikelyLANBroadcastIPAddress_inner(ipv4Addresses):
 
