56ec9e8d4dd04766b8d63c2fbe8feba5a62e844e
jddes <octosigconsulting@gmail.com>
2024-05-16 13:41:40 -0400

    int()

--- digital_servo_python_gui/DataLoggingDisplayWidget.py
+++ digital_servo_python_gui/DataLoggingDisplayWidget.py
@@ -89,7 +89,7 @@ class DataLoggingDisplayWidget(QtGui.QWidget):
                 self.curvesList.append(list())
                 # create our pen: (TODO)
                 (penR, penG, penB) = colors_list[k2 % numColors]
-                pen = QtGui.QPen(QtGui.QColor(penR*255, penG*255, penB*255))
+                pen = QtGui.QPen(QtGui.QColor(int(penR*255), int(penG*255), int(penB*255)))
                 pen.setCosmetic(True)
                 self.curvesList[k2].append(self.pltItemsList[-1].plot(pen=pen))
 
